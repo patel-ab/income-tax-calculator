4f6afd114e6935cf3de565195f24c9c249265288
Abhishek Patel <abhishekvpatel20@gmail.com>
2025-03-07 11:38:04 -0500

    Added income_tax interface

---
+++ sector/income_tax.java
@@ -0,0 +1,33 @@
+package sector;
+
+public interface income_tax
+{
+		/*Scanner sc=new Scanner(System.in);
+			 String name;
+			 String feild;
+			 float income,in_tax,tax;*/
+		
+		/*	income_tax()
+			{
+				name="No Name";
+				feild="No Feild";
+				income=(float)0.0;
+			}*/
+			void get();
+			/*{
+				System.out.println("Enter your name...");
+				name=sc.nextLine();
+			   	System.out.println("Enter your income....");
+				income=sc.nextFloat();
+			}*/
+			void display();
+			/*{
+				System.out.println(".........................");
+				System.out.println("Name is: "+name);
+				System.out.println("Income is: "+income);
+				System.out.println("Tax rate is: "+tax);
+				System.out.println("Tax Amount is "+in_tax);
+				System.out.println(".........................");
+				
+			}*/
+}
